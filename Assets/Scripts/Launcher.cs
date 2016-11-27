a4f52f8e538abebfe03c3da54026792c1fbef963
Joel Delight <thefencingflutist@gmail.com>
2016-11-27 12:43:40 -0800

    get started on transferring objects to mulitplayer

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Launcher : Photon.PunBehaviour {
+
+    string _gameVersion = "1";
+    public PhotonLogLevel LogLevel = PhotonLogLevel.Informational;
+
+    bool isConnecting;
+
+    // Use this for initialization
+    void Awake()
+    {
+        PhotonNetwork.logLevel = LogLevel;
+        PhotonNetwork.autoJoinLobby = false;
+
+        PhotonNetwork.automaticallySyncScene = true;
+    }
+        
+	void Start () {
+
+    }
+
+    public void Connect()
+    {
+        isConnecting = true;
+        if (PhotonNetwork.connected)
+        {
+            // #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnPhotonRandomJoinFailed() and we'll create one.
+            Debug.Log("Join a room.");
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            // #Critical, we must first and foremost connect to Photon Online Server.
+            PhotonNetwork.ConnectUsingSettings(_gameVersion);
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log(isConnecting);
+        if (isConnecting)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+        Debug.Log("DemoAnimator/Launcher: OnConnectedToMaster() was called by PUN");
+    }
+
+
+    public override void OnDisconnectedFromPhoton()
+    {
+        Debug.LogWarning("DemoAnimator/Launcher: OnDisconnectedFromPhoton() was called by PUN");
+    }
+
+    public override void OnPhotonRandomJoinFailed(object[] codeAndMsg)
+    {
+        Debug.Log("Failed to join random, we crate our own one.");
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        
+        Debug.Log("DemoAnimator/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+
+        // #Critical: We only load if we are the first player, else we rely on  PhotonNetwork.automaticallySyncScene to sync our instance scene.
+        if (PhotonNetwork.room.playerCount == 1)
+        {
+            Debug.Log("Yo Yo Yo, made a room.");
+            PhotonNetwork.LoadLevel("Multiplayer");
+        }
+    }
+
+}
---
+++ Assets/Scripts/SelectObject.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class SelectObject : MonoBehaviour {
+    public GameObject selectedObject = null;
+    public Launcher launcher;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Debug.Log(hit.transform.gameObject.name);
+                selectedObject = hit.transform.gameObject;
+                launcher.Connect();
+                DontDestroyOnLoad(selectedObject);
+                selectedObject.AddComponent(Type.GetType("PhotonView"));
+                selectedObject.GetPhotonView().
+            }
+        }
+    }
+}
